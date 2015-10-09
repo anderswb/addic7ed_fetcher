cbb83e77cd387f1ffb6ae721cbcfbdf4b60a8d51
Anders Brandt <anderswb@gmail.com>
2015-10-09 23:09:40 +0200

    changed to return list of tuples

--- fetchandparse.py
+++ fetchandparse.py
@@ -9,8 +9,8 @@ class fetchAndParse:
     def getshows(self):
         """
         Get the list of shows available from the main page of addic7ed
-        :return: A dict with the key being the addic7ed given value of the show, and the value being the name of
-        the show.
+        :return: A list of tuple with the first entry being the addic7ed given value of the show,
+        and the second entry being the name of the show.
         """
         page = requests.get('http://www.addic7ed.com')
         tree = html.fromstring(page.text)
@@ -18,11 +18,13 @@ class fetchAndParse:
         show_values = tree.xpath('//select[@id="qsShow"]/option/@value')
         show_names = tree.xpath('//select[@id="qsShow"]/option/text()')
 
-        shows = {}
+        shows = []
         for i, value in enumerate(show_values):
             if value != '0':
-                shows[int(value)] = show_names[i]
+                shows.append((value, show_names[i]))
+                #shows[int(value)] = show_names[i]
 
+        shows.sort(key=lambda tup: tup[1], reverse=True)
         return shows
 
     def getseasons(self, showvalue):
