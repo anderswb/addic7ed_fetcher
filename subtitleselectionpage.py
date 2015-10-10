919c836b28dd169a184c812304d73545ebdd2d6e
Anders Brandt <anderswb@gmail.com>
2015-10-10 21:22:36 +0200

    Added filter checkbutton, but haven't implemented the functionality

--- subtitleselectionpage.py
+++ subtitleselectionpage.py
@@ -11,6 +11,11 @@ class SubtitleSelectionPage(tk.Frame):
     label1 = None
     notebook = None
 
+    def filterchanged(self):
+        print(self.hivar.get())
+        print(self.hdvar.get())
+        print(self.corvar.get())
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         SubtitleSelectionPage.label1 = ttk.Label(self, text="Unknown show")
@@ -20,12 +25,34 @@ class SubtitleSelectionPage(tk.Frame):
 
         SubtitleSelectionPage.notebook = ttk.Notebook(self)
 
-        SubtitleSelectionPage.label1.grid(row=0, column=0)
-        SubtitleSelectionPage.notebook.grid(row=1, column=0, sticky='nsew')
-        button1.grid(row=2, column=0)
+        self.hivar = tk.BooleanVar(self)
+        self.hdvar = tk.BooleanVar(self)
+        self.corvar = tk.BooleanVar(self)
+
+        check_hi = ttk.Checkbutton(self, text="HI", variable=self.hivar, command=self.filterchanged)
+        check_hd = ttk.Checkbutton(self, text="HD", variable=self.hdvar, command=self.filterchanged)
+        check_cor = ttk.Checkbutton(self, text="Corrected", variable=self.corvar, command=self.filterchanged)
+
+        dropdownvar = tk.StringVar(self)
+        dropdownvar.set("one") # default value
+        dropdown = ttk.OptionMenu(self, dropdownvar, "one", "two", "three")
+
+        SubtitleSelectionPage.label1.grid(row=0, column=0, columnspan=4)
+        SubtitleSelectionPage.notebook.grid(row=1, column=0, columnspan=4, sticky='nsew')
+
+        check_hi.grid(row=2, column=0)
+        check_hd.grid(row=2, column=1)
+        check_cor.grid(row=2, column=2)
+        dropdown.grid(row=2, column=3)
+
+        button1.grid(row=3, column=0)
 
         tk.Grid.grid_columnconfigure(self, 0, weight=1)
+        tk.Grid.grid_columnconfigure(self, 1, weight=1)
+        tk.Grid.grid_columnconfigure(self, 2, weight=1)
+        tk.Grid.grid_columnconfigure(self, 3, weight=1)
         tk.Grid.grid_rowconfigure(self, 1, weight=1)
+
 
     def updatedisplay(self, selectedshow):
         SubtitleSelectionPage.label1.config(text=selectedshow[1])
