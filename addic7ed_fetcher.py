086e02a58f7e1b8049e2f014849d372c96ab0b1b
Anders Brandt <anderswb@gmail.com>
2015-10-10 15:17:50 +0200

    Added notebook with the seasons as tabs

--- addic7ed_fetcher.py
+++ addic7ed_fetcher.py
@@ -94,19 +94,32 @@ class SelectShowPage(tk.Frame):
 
 class SubtitleSelectionPage(tk.Frame):
     label1 = None
+    notebook = None
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         SubtitleSelectionPage.label1 = ttk.Label(self, text="Unknown show")
-        SubtitleSelectionPage.label1.pack(pady=10, padx=10)
 
         button1 = ttk.Button(self, text="Go to page one",
                              command=lambda: controller.show_frame(SelectShowPage))
-        button1.pack()
+
+
+
+        SubtitleSelectionPage.notebook = ttk.Notebook(self)
+
+        SubtitleSelectionPage.label1.grid(row=0, column=0)
+        SubtitleSelectionPage.notebook.grid(row=1, column=0, sticky='nsew')
+        button1.grid(row=2, column=0)
+
+        tk.Grid.grid_columnconfigure(self, 0, weight=1)
+        tk.Grid.grid_rowconfigure(self, 1, weight=1)
 
     def updatedisplay(self, selectedshow):
-        print(SubtitleSelectionPage.test)
         SubtitleSelectionPage.label1.config(text=selectedshow[1])
+        seasons = fetchAndParse.getseasons(self, selectedshow[0])
+        for eachseason in seasons:
+            frame = ttk.Frame(SubtitleSelectionPage.notebook)
+            SubtitleSelectionPage.notebook.add(frame, text="S{:02}".format(eachseason))
 
 
         
