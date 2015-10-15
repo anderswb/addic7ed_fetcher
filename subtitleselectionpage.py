8385f7c3749ea6be7a88378105b22356b0667969
Anders Brandt <anderswb@gmail.com>
2015-10-15 12:43:25 +0200

    Moved filter to separate class, plus some cleanup

--- subtitleselectionpage.py
+++ subtitleselectionpage.py
@@ -1,14 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-from fetchandparse import FetchAndParse
-from tkinter.font import Font
-import selectshowpage
-from downloaddialog import DownloadDialog
-from downloadsession import session
-from tkinter import messagebox
-from re import findall
-from threading import Thread
-import time
 
 import page
 
@@ -21,21 +12,33 @@ class SubtitleSelectionPage(page.Page):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        SubtitleSelectionPage.label1 = ttk.Label(self, text="Unknown show")
+        label1 = ttk.Label(self, text="Unknown show")
+        notebook = ttk.Notebook(self)
+        filterpanel = FilterPanel(self)
+        buttonpanel = page.ButtonPanel(self, buttons=('Download', 'Back'))
 
-        SubtitleSelectionPage.notebook = ttk.Notebook(self)
+        # Packing
+        label1.pack(side=tk.TOP)
+        filterpanel.pack(side=tk.TOP, fill=tk.X)
+        notebook.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        buttonpanel.pack(side=tk.BOTTOM, fill=tk.X)
+
+
+class FilterPanel(tk.Frame):
+
+    def __init__(self, master=None, cnf={}, **kw):
+        tk.Frame.__init__(self, master, cnf, **kw)
 
         # Setup the checkboxes
         self.hivar = tk.StringVar(value='off')
         self.hdvar = tk.StringVar(value='off')
         self.corvar = tk.StringVar(value='off')
 
-        nestedframe = tk.Frame(self)
-        self.check_hi = ttk.Checkbutton(nestedframe, text="HI", variable=self.hivar, command=self.hichanged,
+        self.check_hi = ttk.Checkbutton(self, text="HI", variable=self.hivar, command=self.hichanged,
                                         onvalue='on', offvalue='off')
-        self.check_hd = ttk.Checkbutton(nestedframe, text="HD", variable=self.hdvar, command=self.hdchanged,
+        self.check_hd = ttk.Checkbutton(self, text="HD", variable=self.hdvar, command=self.hdchanged,
                                         onvalue='on', offvalue='off')
-        self.check_cor = ttk.Checkbutton(nestedframe, text="Corrected", variable=self.corvar, command=self.corchanged,
+        self.check_cor = ttk.Checkbutton(self, text="Corrected", variable=self.corvar, command=self.corchanged,
                                          onvalue='on', offvalue='off')
 
         self.hdvar.set(value='dontcare')
@@ -53,24 +56,13 @@ class SubtitleSelectionPage(page.Page):
         # Setup the dropdown menu
         self.dropdownvar = tk.StringVar(self)
         self.dropdownvar.set("All languages")  # default value
-        self.dropdown = ttk.OptionMenu(nestedframe, self.dropdownvar, "All languages")
-
-        # create button panel
-        self.buttonpanel = page.ButtonPanel(self, buttons=('Download', 'Back'))
-        self.buttonpanel.pack(side=tk.BOTTOM, fill=tk.X)
-
-        # Packing
-        SubtitleSelectionPage.label1.pack(side=tk.TOP)
-
-        nestedframe.pack(side=tk.TOP, fill=tk.X)
+        self.dropdown = ttk.OptionMenu(self, self.dropdownvar, "All languages")
 
         self.check_hi.grid(row=0, column=0, sticky='w', padx=10)
         self.check_hd.grid(row=0, column=1, sticky='w', padx=10)
         self.check_cor.grid(row=0, column=2, sticky='w', padx=10)
         self.dropdown.grid(row=0, column=3, sticky='e', padx=10)
-        tk.Grid.grid_columnconfigure(nestedframe, 3, weight=1)
-
-        SubtitleSelectionPage.notebook.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        tk.Grid.grid_columnconfigure(self, 3, weight=1)
 
     def hdchanged(self):
         self.last_hd_state = self.checkboxchange(self.last_hd_state, self.hdvar, self.check_hd)
