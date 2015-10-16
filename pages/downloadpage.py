da9dc888af2612c7404549b722881ddb01f876f4
Anders Brandt <anderswb@gmail.com>
2015-10-16 16:14:41 +0200

    Added widgets

--- pages/downloadpage.py
+++ pages/downloadpage.py
@@ -11,6 +11,13 @@ class DownloadPage(page.Page):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        columns = ['Status', 'Season', 'Episode']
+        self.statustree = ttk.Treeview(self, columns=columns, show='headings')
+        self.statustree.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        for column in columns:
+            self.statustree.heading(column, text=column.title())
+        #self.populatetree(showlist)
+
 
         self.buttonpanel = page.ButtonPanel(self, buttons=('OK', 'Back'),
                                             commands=[controller.selectshowpage_okpressed,
